bd134aca5b61ac2faae9fb258914e9d88ec50539
Grace Huang <gracefh@mit.edu>
2024-04-24 17:34:18 -0400

    make encryptionr eturn value bit string

--- aes_utils.py
+++ aes_utils.py
@@ -1,12 +1,16 @@
 from Crypto.Cipher import AES
 from Crypto.Hash import HMAC, SHA256
 from Crypto.Random import get_random_bytes
+from bitstring import BitArray
 
 def make_key():
     return get_random_bytes(16)
 
 # Implementation based off of: https://pycryptodome.readthedocs.io/en/latest/src/examples.html#encrypt-data-with-aes
 # Encrypts a message, as well as adds an authentication tag to prevent replay attacks? 
+#
+# TODO:
+#  - add message length into encryption, so that when decoding, we can figure out which bits to decode from the audio file
 def encrypt_message(msg, aes_key, hmac_key): 
     data = msg.encode()
 
@@ -21,13 +25,16 @@ def encrypt_message(msg, aes_key, hmac_key):
         f.write(cipher.nonce)
         f.write(ciphertext)
     
-    return tag + cipher.nonce + ciphertext # maybe concatenate these?
+    enc =  tag + cipher.nonce + ciphertext
+    return BitArray(enc).bin # return as bit string
 
 def decrypt_message(full_ciphertext, aes_key, hmac_key):
     
-    tag = full_ciphertext[:32]
-    nonce = full_ciphertext[32:40]
-    ciphertext = full_ciphertext[40:]
+    converted_ct = BitArray(bin=full_ciphertext).bytes
+
+    tag = converted_ct[:32]
+    nonce = converted_ct[32:40]
+    ciphertext = converted_ct[40:]
 
     try:
         hmac = HMAC.new(hmac_key, digestmod=SHA256)
@@ -45,7 +52,7 @@ if __name__=="__main__":
     aes_key = make_key()
     hmac_key = make_key()
     
-    enc = encrypt_message("hello", aes_key, hmac_key)
+    enc = encrypt_message("hello world", aes_key, hmac_key)
     dec = decrypt_message(enc, aes_key, hmac_key)
     
     print("Encrypt returned:", enc)
